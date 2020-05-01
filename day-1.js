136f19bd6d6223f8b38afeeebbdc58818db3efbb
Shubham Ambastha <shubhams.saurav@gmail.com>
2020-05-01 23:35:12 +0530

    day-1 challenge solution uploaded

---
+++ day-1.js
@@ -0,0 +1,21 @@
+// Question: First Bad Version
+
+var solution = function (isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function (n) {
+        let left = 1;
+        let right = n;
+        while (left < right) {
+            let mid = Math.floor(left + (right - left) / 2)
+            if (!isBadVersion(mid)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    };
+};
